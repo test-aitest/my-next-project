a4505df07a30b23a318231eb7d82423c89ce31e0
test-aitest <tatukidayo@icloud.com>
2024-10-22 20:50:16 +0900

    ニュースにSSRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -5,11 +5,7 @@ import { NEWS_LIST_LIMIT } from "@/app/_constraints";
 import Pagination from "@/app/_components/Pagination";
 import SearchField from "@/app/_components/SearchField";
 
-type Props = {
-  params: {
-    current: string;
-  };
-};
+export const revalidate = 0;
 
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
